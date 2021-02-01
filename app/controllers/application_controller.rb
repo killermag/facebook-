d5eccef9280bbd9eaa36030d1a6e74251e098444
killermag <galahaal580@gmail.com>
2021-02-01 17:14:40 -0500

    added fields to user + styling

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,16 @@ class ApplicationController < ActionController::Base
   before_action :set_username_from_first_and_last, if: :making_a_new_user 
   before_action :permit_params, if: :devise_controller?
 
+
   private 
+
+  def after_sign_up_path_for(resource)
+  	new_user_sessions_path
+  end 
+
+  def after_sign_in_path_for(resource)
+ 	  homes_path
+  end
 
   def self.find_for_database_authentication warden_condition
     conditions = warden_condition.dup
@@ -20,22 +29,15 @@ class ApplicationController < ActionController::Base
 
   def set_username_from_first_and_last
     if params[:user]
-    params[:user][:username] = "#{params[:user][:first]}#{params[:user][:last]}"
+    params[:user][:username] = "#{params[:user][:first]}_#{params[:user][:last]}"
     params[:user].delete(:first)
     params[:user].delete(:last)
     end 
   end 
 
-  def after_sign_up_path_for(resource)
-  	homes_path
-  end 
-
-  def after_sign_in_path_for(resource)
- 	homes_path
-  end
 
   def permit_params 
-    devise_parameter_sanitizer.permit(:sign_up) { |user| user.permit(:username, :password,:email)}
+    devise_parameter_sanitizer.permit(:sign_up) { |user| user.permit(:username, :password,:email, :gender, "birthday(3i)","birthday(2i)","birthday(1i)" ) }
     devise_parameter_sanitizer.permit(:sign_in, keys: [:login, :password, :password_confirmation])
   end 
 end
