0961836df8477d47c0d9a97e773be915991a69d1
killermag <galahaal580@gmail.com>
2021-02-01 21:52:16 -0500

    changed up views and stuff..

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,14 @@
 Rails.application.routes.draw do
-  
+
+  get 'user', to: 'users#me', as: 'current_user'
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
 
   devise_scope :user do 
     root "devise/sessions#new"
 
   end 
-
+  
   resources :homes, only: :index
+  
 
 end 
