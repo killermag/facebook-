182c9c4dfe51bf9ab9a09a695fd578a19959d4fe
killermag <galahaal580@gmail.com>
2021-02-10 12:41:56 -0500

    likes controller modifications

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,4 +1,29 @@
 class LikesController < ApplicationController
   def create 
+    params = CGI.parse(URI.parse(request.url).query)
+    # Check if the post is already liked 
+    # Dynamically create like and unlike post links 
+    # catch error below. 
+    if params["post"].to_i
+      post = Post.find(params["post"]) 
+      if post 
+        @like = Like.new(user: current_user, post: post)
+        if @like.save 
+          flash[:success] = "Liked post."
+          redirect_to homes_path
+        else 
+          flash.now[:error] = "Unable to process request."
+          render "homes/index"
+        end 
+      else 
+      end 
+    else 
+      flash.now[:error] = 'There was an error processing the URL. Try again'
+      render "homes/index"
+    end 
   end 
+
+  def destroy 
+  end 
+  
 end
