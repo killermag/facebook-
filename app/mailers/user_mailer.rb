b85e1b41c1c41863c70d579fe1ac7c41dac78b03
killermag <galahaal580@gmail.com>
2021-02-22 19:40:53 -0500

    changed mailer default from and default host in production.rb

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,8 +1,9 @@
 class UserMailer < ApplicationMailer
+  default from: 'odinairs80@gmail.com'
   
   def after_sign_up_email 
     @user = params[:user]
-    @url = 'http://localhost:3000'
+    @url = 'https://morning-beach-48118.herokuapp.com'
     mail(to: @user.email, subject: 'Thank you for signing up on odin based facebook.')
   end 
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -121,6 +121,8 @@ Rails.application.configure do
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { host: "https://morning-beach-48118.herokuapp.com" }
+
 
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
